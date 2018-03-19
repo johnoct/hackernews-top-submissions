dd9066898845204fb1fabd566fc98fb02081be48
John Octubre <johnoctubre7@gmail.com>
2018-03-18 23:07:41 -0400

    update

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 import requests
 import json
-import flask
-from multiprocessing.dummy import Pool as ThreadPool
 from operator import itemgetter
 from flask import Flask
 app = Flask(__name__)
@@ -16,10 +14,7 @@ def hn_submissions():
     # Process information about each submission.
     submission_ids = r.json()
     submission_dicts = []
-    pool = ThreadPool(12) 
-
-    # function
-    def get_submission(submission_id):
+    for submission_id in submission_ids[:30]:
         # Make a seperate API call for each submission.
         url = ('https://hacker-news.firebaseio.com/v0/item/' +
                str(submission_id) + '.json')
@@ -32,9 +27,8 @@ def hn_submissions():
             'comments': response_dict.get('descendants', 0),
         }
         submission_dicts.append(submission_dict)
-    pool.map(get_submission, submission_ids[:15])
 
     submission_dicts = sorted(
         submission_dicts, key=itemgetter('comments'), reverse=True)
 
-    return flask.jsonify(submission_dicts)
+    return json.dumps(submission_dicts, indent=4)
