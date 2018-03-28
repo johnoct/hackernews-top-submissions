218b0d781d65dd04f0b676e57f726a398f1092e8
John Octubre <johnoctubre7@gmail.com>
2018-03-27 22:12:43 -0400

    update concurrent calls

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import flask
-from multiprocessing.dummy import Pool as ThreadPool
+from concurrent.futures import ProcessPoolExecutor
+from requests import Session
+from requests_futures.sessions import FuturesSession
 from operator import itemgetter
 from flask import Flask
 app = Flask(__name__)
@@ -9,32 +11,43 @@ app = Flask(__name__)
 
 @app.route('/top_hn_subs')
 def hn_submissions():
+    
+    session = FuturesSession(executor=ProcessPoolExecutor(max_workers=10),
+                         session=Session())
+
     # Make an API call and store the response.
     url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
     r = requests.get(url)
 
+    
     # Process information about each submission.
     submission_ids = r.json()
     submission_dicts = []
-    pool = ThreadPool(12) 
 
-    # function
-    def get_submission(submission_id):
-        # Make a seperate API call for each submission.
-        url = ('https://hacker-news.firebaseio.com/v0/item/' +
-               str(submission_id) + '.json')
-        submission_r = requests.get(url)
-        response_dict = submission_r.json()
+    # Craft an array of URLS
+    urls = []
+    for submission in submission_ids[:30]:
+        urls.append('https://hacker-news.firebaseio.com/v0/item/{}.json'.format(submission))
 
+    sessions = []
+    for url in urls:
+        sessions.append(session.get(url))
+
+    responses = []
+    for session in sessions:
+        responses.append(session.result())      
+
+    for r in responses:
+        response_dict = r.json()
         submission_dict = {
-            'title': response_dict['title'],
-            'link': 'https://news.ycombinator.com/item?id=' + str(submission_id),
+            'title': response_dict.get('title', ''),
+            'link': 'https://news.ycombinator.com/item?id={}'.format(response_dict.get('id', 0)),
             'comments': response_dict.get('descendants', 0),
+            'score': response_dict.get('score', 0),
         }
         submission_dicts.append(submission_dict)
-    pool.map(get_submission, submission_ids[:15])
 
     submission_dicts = sorted(
-        submission_dicts, key=itemgetter('comments'), reverse=True)
+        submission_dicts, key=itemgetter('score'), reverse=True)
 
     return flask.jsonify(submission_dicts)
